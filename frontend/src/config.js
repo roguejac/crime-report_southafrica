70a737cbf66ffaac2a51ba8c03d1c2f289505226
roguejac <46568234+roguejac@users.noreply.github.com>
2025-06-10 15:18:07 +0200

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,6 @@
 // config.js
+const isProduction = import.meta.env.PROD;
+
 export const API_BASE = "https://crime-report-southafrica-1.onrender.com";
+  ? "https://your-backend-name.onrender.com"
+  : "http://localhost:8000";
