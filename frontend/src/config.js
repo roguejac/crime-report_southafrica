41d52a952a09146d01be295bfdfd4307abe7c85d
roguejac <46568234+roguejac@users.noreply.github.com>
2025-06-10 15:57:27 +0200

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,5 +2,5 @@
 const isProduction = import.meta.env.PROD;
 
 export const API_BASE_URL = isProduction
-  ? "https://crime-report-southafrica-1.onrender.com"
+  ? "https://crime-report-southafrica.onrender.com"
   : "http://localhost:8000";
