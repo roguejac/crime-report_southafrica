bee3eaf054ad1cf25e2af0a20d643951db7808d0
roguejac <46568234+roguejac@users.noreply.github.com>
2025-06-13 12:29:12 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,10 @@
+// frontend/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000 // Set higher than the default 500kB
+  }
 })
